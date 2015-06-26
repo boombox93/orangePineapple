9efa294ade2d413574fa78851f191fea4eda0f15
boombox93 <taylor.engels@gmail.com>
2015-06-25 21:08:51 -0400

    Worked on Main
    T.

--- src/orangePineapple/Main.java
+++ src/orangePineapple/Main.java
@@ -1,10 +1,54 @@
 package orangePineapple;
 
-public class Main {
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class Main implements Runnable{
+
+	@Override
+	public void run() 
+	{
+		// Create GUI window
+		// **Add items to JPanel not JFrame, the JFrame size is being set by JPanel**
+		JFrame jf = new JFrame("Hi");
+		JPanel jp = new JPanel();
+		jp.setPreferredSize(new Dimension(800, 800));
+		jf.getContentPane().add(jp);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setLayout(new FlowLayout());
+		
+		// Items added below
+		
+		JTextArea ta = new JTextArea(10, 20);
+		Dimension d = new Dimension(10, 24);
+		jp.add(ta);
+		ta.setPreferredSize(d);
+		
+		//jp.add(new JLabel("Hello, world!"));
+		//jp.add(new JButton("Button"));
+		
+		// Items displayed on JFrame below
+		jf.pack();
+		jf.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		Main m = new Main();
+		// Event queue
+		SwingUtilities.invokeLater(m);
 
 	}
+
+	
 
 }
