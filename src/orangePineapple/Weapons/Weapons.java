1d0dc4546ddb9bd97e38b079d72f4d346a93c51d
pyright <camposmike92@gmail.com>
2015-07-11 13:06:11 -0500

    .

---
+++ src/orangePineapple/Weapons/Weapons.java
@@ -0,0 +1,14 @@
+package orangePineapple.Weapons;
+
+public class Weapons
+{
+private int damageRate; //The rate at which the weapon deals damage.
+private int damage; //The amount of damage the weapon deals.
+private String damageType; //The type of damage the weapon deals (fire, melee, explosive, etc.)
+private int durability; //The amount of use an item can handle
+private int attackRange; //The distance at which the weapon can hit
+    
+public Weapon() //Constructor
+    { 
+    }
+};
---
+++ src/orangePineapple/Weapons/myPistol.java
@@ -0,0 +1,17 @@
+package orangePineapple.Weapons;
+
+public class myPistol extends Weapons {
+
+	//Constructor
+	public myPistol (int damageRate, int damage, String damageType, int durability, int attackRange)
+	{
+		damageRate = 1;
+		damage = 10;
+		damageType = "Kinetic";
+		durability = 1000;
+		attackRange = 1;
+	}
+	
+	
+	
+}
